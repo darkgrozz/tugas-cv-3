ac2c4a84a04e6beedd77e0de90ba7e82479257a6
darkgrozz <darkgroz046@gmail.com>
2021-11-05 14:22:51 +0700

    rizky

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" class="container">
+    <div class="row py-4">
+      <CvDua />
+      <CvSatu />
+    </div>
+  </div>
+</template>
+
+<script>
+  import CvDua from './components/CvSatu.vue'
+  import CvSatu  from './components/CvDua.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      CvDua,
+      CvSatu,
+    }
+  }
+</script>
